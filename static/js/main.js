a88e868e9a1f878542496a110c66da94d6096cab
Kyle Roux <kyle@casepeer.com>
2017-05-16 10:56:17 -0700

    css fix and js update

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,17 @@
 $('[data-toggle=confirmation]').confirmation({
-  	rootSelector: '[data-toggle=confirmation]',
-	//btnOkLabel: 'Si',
-	//btnCancelLabel: 'No'
+    rootSelector: '[data-toggle=confirmation]',
+    btnOkLabel: 'Ok',
+    btnCancelLabel: 'No',
+    'placement': 'top',
+});
+var dismissAlerts = function() {
+    var alertElements = Array(document.querySelectorAll(".alert"));
+    alertElements.forEach(function(e) {
+        setTimeout(function() {
+            $(e).remove();
+        }, 2500)
+    });
+};
+$(document).ready(function() {
+    dismissAlerts();
 });
